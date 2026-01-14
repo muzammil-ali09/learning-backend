a8f5c0adf31937be3f25b19734116741cd7fb721
muzammil-ali09 <muzuali295@Gmail.com>
2026-01-14 16:35:07 +0500

    today final work

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js"; 
-import dotenv from "dotenv"
+
 
 const connectDB=async ()=>{
     try{
-      const  connectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-     console.log(`\n mongoDB connected !! DB Host:${connectionInstance.connection.host}`)
+     const  connectionInstance= await mongoose.connect
+     (`${process.env.MONGODB_URL}/${DB_NAME}`)
+     console.log(`\n mongoDB connected !! DB Host:
+        ${connectionInstance.connection.host}`);
     }catch(error){
         console.log("MONGODB connection error",error );
         process.exit(1)
     }
 } 
  
-export default connectDB()
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import dotenv from "dotenv"
 
-import mongoose  from "mongoose"; 
-
-import { DB_NAME } from "./constant.js";
-import express  from "express";
 import  connectDB  from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
- 
+
 connectDB()
 
 
