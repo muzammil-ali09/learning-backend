5969a7782eeeade1882c1e111b37c776bba1abf8
muzammil-ali09 <muzuali295@Gmail.com>
2026-01-15 16:38:54 +0500

    15 jan

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,60 @@
- a
+import mongoose ,{Schema, SchemaTypes} from "mongoose"; 
+
+
+const userSchema=new Schema ({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String, // cloudinary 
+        required:true,
+
+    },
+    coverImage:{
+        type:String, // cloudinary  
+
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true ,"password is required"]
+    },
+    refreshToken:{
+        type:String,
+    }
+
+},
+{
+    timestamps:true,
+}) 
+
+
+
+
+
+
+
+ export  const  User = mongoose.model("User",userSchema)
