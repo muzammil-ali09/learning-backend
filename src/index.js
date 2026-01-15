a1311be1ef091f4870973e56514fcd5827878355
muzammil-ali09 <muzuali295@Gmail.com>
2026-01-15 15:48:46 +0500

    15 jan

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"
-
+import app  from "./app.js";
 import  connectDB  from "./db/index.js";
 
 dotenv.config({
@@ -7,7 +7,14 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection fail", err)
+})
 
 
 
