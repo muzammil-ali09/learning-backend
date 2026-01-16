e5b1a8486c4df237d4d45dde7d774202a2dc377d
muzammil-ali09 <muzuali295@Gmail.com>
2026-01-16 16:02:28 +0500

    16 jan

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose ,{Schema, SchemaTypes} from "mongoose"; 
+import  jsonwebtoken from "jsonwebtoken";
+import  bcrypt  from "bcrypt"
+
 
 
 const userSchema=new Schema ({
@@ -52,9 +55,17 @@ const userSchema=new Schema ({
 }) 
 
 
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
 
+     this.password=await bcrypt.hash(this.password,10)
+     next()
+})
 
-
+ userSchema.methods.isPasswordCorrect= async function(password){
+   return await bcrypt.compare(password,this.password) 
+ }
+ 
 
 
  export  const  User = mongoose.model("User",userSchema)
