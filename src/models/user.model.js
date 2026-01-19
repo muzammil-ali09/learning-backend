f58839f82cb05155a334a18a0c5db1d1a2ca19de
muzammil-ali09 <muzuali295@Gmail.com>
2026-01-19 15:44:39 +0500

    19 jan

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema=new Schema ({
         lowercase:true,
         trim:true,
     },
-    fullname:{
+    fullName:{
         type:String,
         required:true,
         trim:true,
@@ -66,6 +66,33 @@ userSchema.pre("save", async function(next){
    return await bcrypt.compare(password,this.password) 
  }
  
+ userSchema.methods.generateAccessToken=function(){
+  return jsonwebtoken.sign(
+    {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
 
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+ }
 
+ userSchema.methods.generateRefreshToken=function(){
+return jsonwebtoken.sign(
+    {
+        _id:this._id,
+        
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+ }
  export  const  User = mongoose.model("User",userSchema)
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -14,7 +14,7 @@ class ApiError extends Error{
        if(stack){
          this.stack=stack
        }else {
-        error.captureStackTrace(this.this
+        error.captureStackTrace(this,this
             .constructor)
        }
    
